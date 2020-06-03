6a67a0f3289a557c023086b5265acd8ca6e976af
christopherkim228 <64342451+christopherkim228@users.noreply.github.com>
2020-06-04 01:27:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 //VARIABLE
 let isBasic = true;
+const hard = [
+              ["수소", "H"],
+              ["리튬", "Li"],
+              ["나트륨", "Na"],
+              ["칼륨", "K"],
+              ["마그네슘", "Mg"],
+              ["붕소", "B"],
+              ["알루미늄", "Al"],
+              ["탄소", "C"]
+             ]
 
-//INITIAL
+
 function showTables() {
     if (isBasic) {
         
     } else {
-
+        
     }
 }
+
+function arrayToTable(arr, tablePath) {
+    let dest = document.getElementById(tablePath)
+    arr.forEach(function(e){
+        //show
+    })
+
+}
+
