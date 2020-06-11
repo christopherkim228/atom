db4003d52c3b292145e197abe2309eacad1517ae
christopherkim228 <64342451+christopherkim228@users.noreply.github.com>
2020-06-11 15:55:52 +0900

    created hard/easy

--- app.js
+++ app.js
@@ -2,15 +2,85 @@
 let isBasic = true;
 const basic = {
     elements: [
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["", ""],
+        ["수소", "H"],
+        ["리튬", "Li"],
+        ["나트륨", "Na"],
+        ["칼륨", "K"],
+        ["마그네슘", "Mg"],
+        ["붕소", "B"],
+        ["알루미늄", "Al"],
+        ["탄소", "C"],
+        ["은", "Ag"],
+        ["금", "Au"],
+        ["수은", "Hg"],
+        ["철", "Fe"],
+        ["구리", "Cu"],
+        ["규소", "Si"],
+        ["질소", "N"],
+        ["납", "Pb"],
+        ["인", "P"],
+        ["산소", "O"],
+        ["황", "S"],
+        ["플루오린", "F"],
+        ["염소", "Cl"],
+        ["아이오딘", "I"],
+        ["헬륨", "He"],
+        ["네온", "Ne"]
     ],
-    molecules: [],
+    molecules: [
+        ["수소", "H<sub>2</sub>"],
+        ["염화수소", "HCl"],
+        ["메테인", "CH<sub>4</sub>"],
+        ["질소", "N<sub>2</sub>"],
+        ["암모니아", "NH<sub>3</sub>"],
+        ["이산화탄소", "CO<sub>2</sub>"],
+        ["일산화탄소", "CO"],
+        [" ", " "],
+        ["물", "H<sub>2</sub>O"],
+        ["산소", "O<sub>2</sub>"],
+        ["과산화수소", "H<sub>2</sub>O<sub>2</sub>"],
+        [" ", " "]
+    ],
+    ions: [
+        ["수소이온", "H<sup>+</sup>"],
+        ["리튬이온", "Li<sup>+</sup>"],
+        ["은이온", "Ag<sup>+</sup>"],
+        ["나트륨 이온", "Na<sup>+</sup>"],
+        ["칼륨이온", "K<sup>+</sup>"],
+        ["마그네슘 이온", "Mg<sup>2+</sup>"],
+        ["칼슘이온", "Ca<sup>2+</sup>"],
+        ["납이온", "Pb<sup>2+</sup>"],
+        ["구리이온", "Cu<sup>2+</sup>"],
+        ["알루미늄 이온", "Al<sup>3+</sup>"],
+        ["암모늄 이온", "NH<sub>4</sub><sup>+</sup>"],
+        ["산화이온", "O<sup>2-</sup>"],
+        ["염화이온", "Cl<sup>-</sup>"],
+        ["아이오딘화 이온", "I<sup>-</sup>"],
+        ["수산화 이온", "OH<sup>-</sup>"],
+        ["질산이온", "NO<sub>3</sub><sup>-</sup>"],
+        ["탄산이온", "CO<sub>3</sub><sup>2-</sup>"],
+        ["황산이온", "SO<sub>4</sub><sup>2-</sup>"],
+        ["황화이온", "S<sup>2-</sup>"],
+        ["아세트산 이온", "CH<sub>3</sub>OO<sup>-</sup>"],
+        ["",""]
+    ],
+    precipitates: [
+        ["염화 은", "Ag<sup>+</sup>", "Cl<sup>-</sup>", "AgCl", "흰색"],
+        [
+            "아이오딘화 납",
+            "Pb<sup>2+</sup>",
+            "I<sub>2</sub><sup>2-</sup>",
+            "PbI<sub>2</sub>",
+            "노란색"
+        ],
+        [
+            "탄산 칼슘",
+            "Ca<sup>2+</sup>",
+            "CO<sub>3</sub><sup>2-</sup>",
+            "CaCO<sub>3</sub>",
+            "흰색"
+        ]
+    ],
 };
 const hard = {
     elements: [
@@ -178,9 +248,14 @@ const hard = {
 };
 //<sub>2</sub><sup></sup>
 
-function showTables(isBasic) {
-    if (isBasic) {
 
+
+function showTables(_isBasic) {
+    if (_isBasic) {
+        elementsToTableBasic(basic.elements, "element");
+        moleculesToTableBasic(basic.molecules, "molecule");
+        ionsToTableBasic(basic.ions, "ion");
+        precipitatesToTableBasic(basic.precipitates, "precipitate");
     } else {
         elementsToTableHard(hard.elements, "element");
         moleculesToTableHard(hard.molecules, "molecule");
@@ -188,6 +263,190 @@ function showTables(isBasic) {
         precipitatesToTableHard(hard.precipitates, "precipitate");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function clearTables() {
+    document.getElementById("element").innerHTML = "";
+    document.getElementById("molecule").innerHTML = "";
+    document.getElementById("ion").innerHTML = "";
+    document.getElementById("precipitate").innerHTML = "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/************************ BASIC **********************/
+
+function elementsToTableBasic(arr, id) {
+    let dest = document.getElementById(id);
+    dest.innerHTML = "<tr><th>원소 이름</th><th>원소 기호</th><th>원소 이름</th><th>원소 기호</th><th>원소 이름</th><th>원소 기호</th></tr>";
+    for (let i = 0; i < 8; i++) {
+        let r = document.createElement("tr");
+        for (let j = 0; j < 3; j++) {
+            let d = document.createElement("td");
+            d.className = "basic-element-name-td"
+            let s = document.createElement("span");
+            s.className = "basic-element-name-span"
+            s.innerHTML = arr[i + j * 8][0];
+            d.appendChild(s);
+            r.appendChild(d);
+            let d1 = document.createElement("td");
+            d1.className = "basic-element-symbol-td"
+            let s1 = document.createElement("span");
+            s1.className = "basic-element-symbol-span"
+            s1.innerHTML = arr[i + j * 8][1];
+            d1.appendChild(s1);
+            r.appendChild(d1);
+        }
+        dest.appendChild(r);
+    }
+}
+
+function moleculesToTableBasic(arr, id) {
+    let dest = document.getElementById(id);
+    dest.innerHTML =
+        "<tr><th>분자 이름</th><th>분자 기호</th><th>분자 이름</th><th>분자 기호</th><th>분자 이름</th><th>분자 기호</th></tr>";
+    for (let i = 0; i < 4; i++) {
+        let r = document.createElement("tr");
+        for (let j = 0; j < 3; j++) {
+            let d = document.createElement("td");
+            let s = document.createElement("span");
+            s.innerHTML = arr[i + j * 4][0];
+            d.appendChild(s);
+            r.appendChild(d);
+            let d1 = document.createElement("td");
+            let s1 = document.createElement("span");
+            s1.innerHTML = arr[i + j * 4][1];
+            d1.appendChild(s1);
+            r.appendChild(d1);
+        }
+        dest.appendChild(r);
+    }
+}
+
+function ionsToTableBasic(arr, id) {
+    let dest = document.getElementById(id);
+    dest.innerHTML =
+        "<tr><th>이온</th><th>이온식</th><th>이온</th><th>이온식</th><th>이온</th><th>이온식</th></tr>";
+    for (let i = 0; i < 7; i++) {
+        let r = document.createElement("tr");
+        for (let j = 0; j < 3; j++) {
+            let d = document.createElement("td");
+            let s = document.createElement("span");
+            s.innerHTML = arr[i + j * 7][0];
+            d.appendChild(s);
+            r.appendChild(d);
+            let d1 = document.createElement("td");
+            let s1 = document.createElement("span");
+            s1.innerHTML = arr[i + j * 7][1];
+            d1.appendChild(s1);
+            r.appendChild(d1);
+        }
+        dest.appendChild(r);
+    }
+}
+
+function precipitatesToTableBasic(arr, id) {
+    let dest = document.getElementById(id);
+    dest.innerHTML = "<tr><th>앙금</th><th>화학 반응식</th><th>앙금의 색</th>";
+    for (let i = 0; i < 3; i++) {
+        let r = document.createElement("tr");
+        let d = document.createElement("td"),
+            s = document.createElement("span");
+        s.innerHTML = arr[i][0];
+        d.appendChild(s);
+        r.appendChild(d);
+        let d1 = document.createElement("td"),
+            s1 = document.createElement("span");
+        s1.innerHTML = arr[i][1];
+        d1.appendChild(s1);
+        if (s1.innerHTML) {
+            let s2 = document.createElement("span");
+                s2.innerHTML = " + ";
+            d1.appendChild(s2);
+        }
+        let s3 = document.createElement("span");
+        s3.innerHTML = arr[i][2];
+        d1.appendChild(s3);
+        if (s1.innerHTML) {
+            let s4 = document.createElement("span");
+            s4.innerHTML = " → ";
+            d1.appendChild(s4);
+        }
+        let s5 = document.createElement("span");
+        s5.innerHTML = arr[i][3] + "↓";
+        d1.appendChild(s5);
+        r.appendChild(d1);
+        let d6 = document.createElement("td"),
+            s6 = document.createElement("span");
+        s6.innerHTML = arr[i][4];
+        d6.appendChild(s6);
+        r.appendChild(d6);
+        dest.appendChild(r);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/************************ HARD **********************/
+
 
 function elementsToTableHard(arr, id) {
     let dest = document.getElementById(id);
@@ -306,7 +565,34 @@ function precipitatesToTableHard(arr, id) {
     }
 }
 
-showTables(false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementById("menu-button").addEventListener("click", function () {
     let icon = document.getElementById('menu-icon');
@@ -317,3 +603,23 @@ document.getElementById("menu-button").addEventListener("click", function () {
         icon.classList.add('cross')
     }, 300))
 });
+
+
+document.getElementById("hard-switch").addEventListener("change", function() {
+    clearTables();
+    if (!this.checked) {
+        showTables(true);
+        console.log("기본 모드");
+    } else {
+        showTables(false);
+        console.log("심화 모드");
+    }
+});
+
+
+
+
+
+
+document.getElementById("hard-switch").checked = false;
+showTables(true);
